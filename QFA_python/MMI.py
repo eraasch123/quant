6ee49afd5c379cb0aec1d537c11d8fcebc9d082b
eraasch123 <raaschevan123@gmail.com>
2025-04-19 17:16:26 -0600

    fixing plot

--- QFA_python/MMI.py
+++ QFA_python/MMI.py
@@ -64,6 +64,38 @@ def generate_portfolios(returns):
         portfolio_risks.append(np.sqrt(np.dot(w.T, np.dot(returns.cov() * NUM_TRADING_DAYS, w))))
     return np.array(portfolio_weights), np.array(portfolio_means), np.array(portfolio_risks)
 
+def statistics(weights, returns):
+    portfolio_return = np.sum(returns.mean() * weights) * NUM_TRADING_DAYS
+    portfolio_volatility = np.sqrt(np.dot(weights.T, np.dot(returns.cov() * NUM_TRADING_DAYS, weights)))
+
+    return np.array([portfolio_return, portfolio_volatility,
+                     portfolio_return / portfolio_volatility])
+    
+# scipy optimize module can find min of given function
+def min_function_sharpe(weights, returns):
+    return -statistics(weights, returns)[2]
+
+def optimize_portfolio(weights, returns):
+    constraints = {'type': 'eq', 'fun': lambda x: np.sum(x) - 1}
+    bounds = tuple((0, 1) for _ in range(len(stocks)))
+    return optimization.minimize(fun=min_function_sharpe, x0=weights[0], args=returns,
+                                            method='SLSQP', bounds=bounds, constraints=constraints)
+
+def print_optimal_portfolio(optimum, returns):
+    print("Optimal portfolio: ", optimum['x'],round(3))
+    print("Expected return, volatility and Sharpe ratio: ",
+          statistics(optimum['x'].round(3), returns))
+
+def show_optimal_portfolio(opt, rets, portfolio_rets, portfolio_vols):
+    plt.figure(figsize=(10,6))
+    plt.scatter(portfolio_vols, portfolio_rets, c=portfolio_rets/portfolio_vols, marker='o')
+    plt.grid(True)
+    plt.xlabel("Expected Volatility")
+    plt.ylabel("Expected Return")
+    plt.colorbar(label="Sharpe Ratio")
+    plt.plot(statistics(opt['x'], rets)[1], statistics(opt['x'], rets)[0], 'g*', markersize=20)
+    plt.show()
+
 
 if __name__ == '__main__':
 
@@ -71,7 +103,9 @@ if __name__ == '__main__':
     print(stock_data)
     calculate_return(stock_data)
     log_daily_returns = calculate_return(stock_data)
-    show_statistics(log_daily_returns)
+    # show_statistics(log_daily_returns)
 
-    weights, means, risks = generate_portfolios(log_daily_returns)
-    show_portfolios(means, risks)
+    pweights, means, risks = generate_portfolios(log_daily_returns)
+    # show_portfolios(means, risks)
+    optimum = optimize_portfolio(pweights, log_daily_returns)
+    show_optimal_portfolio(optimum, log_daily_returns, means, risks)
